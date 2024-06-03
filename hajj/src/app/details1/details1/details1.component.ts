d37b83682937156d04dceedccc781c6507f7be72
TEMOwant2Pee <haythemtahar065@gmail.com>
2024-06-03 10:08:19 +0800

    init

---
+++ hajj/src/app/details1/details1/details1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details1',
+  standalone: true,
+  imports: [],
+  templateUrl: './details1.component.html',
+  styleUrl: './details1.component.css'
+})
+export class Details1Component {
+
+}
