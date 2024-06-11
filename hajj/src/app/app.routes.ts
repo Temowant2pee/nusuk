eed96fc4af63006c78c8b68bce1da2d795a70cc8
TEMOwant2Pee <haythemtahar065@gmail.com>
2024-06-12 01:09:23 +0800

    WIP

--- hajj/src/app/app.routes.ts
+++ hajj/src/app/app.routes.ts
@@ -6,9 +6,12 @@ import {Details3Module} from "./details3/details3.module";
 import {NusukModule} from "./nusuk/nusuk.module";
 
 export const routes: Routes = [
-  { path: 'details1', loadChildren: () => Details1Module},
-  { path: 'details2', loadChildren: () => Details2Module},
-  { path: 'details3', loadChildren: () => Details3Module},
+    { path: 'haj.go.v.sa/en-US/card/id21684443001versiondhash139241faf0e442729b0ef45cc59122cc6452f9aa3a3b3ee877651d5af34a90aa',
+    loadChildren: () => Details1Module},
+  { path: 'haj.go.v.sa/en-US/card/id21684443002versiondhash139241faf0e442729b0ef45cc59122cc6452f9aa3a3b3ee877651d5af34a90aa',
+    loadChildren: () => Details2Module},
+  { path: 'haj.go.v.sa/en-US/card/id21684443003versiondhash139241faf0e442729b0ef45cc59122cc6452f9aa3a3b3ee877651d5af34a90aa',
+    loadChildren: () => Details3Module},
   { path: 'nusuk', loadChildren: () => NusukModule},
   { path: '', redirectTo: '/nusuk', pathMatch: 'full' },
   { path: '**', redirectTo: '/nusuk' }
